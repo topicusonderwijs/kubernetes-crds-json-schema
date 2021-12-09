292e3312555a6ef18f3771dbc435a569cbfda702
Sverre Boschman <1142569+sboschman@users.noreply.github.com>
2021-12-09 18:00:26 +0100

    Add actions step name

--- build.sh
+++ build.sh
@@ -6,8 +6,8 @@ get_latest_release() {
   curl --silent "https://api.github.com/repos/$1/releases/latest" | jq -r .tag_name
 }
 
-CERTMANAGER_VERSION=$(get_latest_release "jetstack/cert-manager" )
-TRAEFIK_VERSION=$(get_latest_release "traefik/traefik" )
+CERTMANAGER_VERSION=$(get_latest_release "jetstack/cert-manager")
+TRAEFIK_VERSION=$(get_latest_release "traefik/traefik")
 
 SCHEMAS=(
   https://github.com/jetstack/cert-manager/releases/download/${CERTMANAGER_VERSION}/cert-manager.crds.yaml
